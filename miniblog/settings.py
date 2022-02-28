3107520a8cf58647687290d197c24a1d85d69bf2
Akash Mathur <mathurakash700@gmail.com>
2022-02-28 23:05:04 +0530

    first commithost

--- miniblog/settings.py
+++ miniblog/settings.py
@@ -24,9 +24,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-qw(q@7!g#ttm6zoy#6*-^3t^oonw$u8bm^w4xc(!rx7g)kzfp2'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
