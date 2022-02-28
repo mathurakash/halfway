67953ac4855e830f1e86190f16133f2b28aa1f6b
Akash Mathur <mathurakash700@gmail.com>
2022-02-28 22:58:32 +0530

    first commit1

--- miniblog/urls.py
+++ miniblog/urls.py
@@ -1,21 +1,14 @@
-"""miniblog URL Configuration
-
-The `urlpatterns` list routes URLs to views. For more information please see:
-    https://docs.djangoproject.com/en/3.2/topics/http/urls/
-Examples:
-Function views
-    1. Add an import:  from my_app import views
-    2. Add a URL to urlpatterns:  path('', views.home, name='home')
-Class-based views
-    1. Add an import:  from other_app.views import Home
-    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
-Including another URLconf
-    1. Import the include() function: from django.urls import include, path
-    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
-"""
 from django.contrib import admin
 from django.urls import path
 from blog import views
+from django.urls import path,include
+from django.views.static import serve as mediaserve
+from django.conf import settings
+from django.conf.urls.static import static
+from django.conf.urls import url
+from django.contrib import admin
+
+
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('',views.home),
@@ -30,3 +23,9 @@ urlpatterns = [
     path('delete_post/<int:pk>',views.delete_post,name="deletepost"),
 
 ]
+
+urlpatterns.append(url(f'^{settings.MEDIA_URL.lstrip("/")}(?P<path>.*)$', mediaserve, {'document_root': settings.MEDIA_ROOT}))
+
+
+if settings.DEBUG: # static
+    urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
