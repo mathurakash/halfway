6aea4bf5b29d8a30293e69ed04d13d4fa0d405c8
Akash Mathur <mathurakash700@gmail.com>
2022-03-01 09:14:25 +0530

    static files

--- miniblog/wsgi.py
+++ miniblog/wsgi.py
@@ -10,18 +10,7 @@ https://docs.djangoproject.com/en/3.2/howto/deployment/wsgi/
 
 import os
 
-
-
 from django.core.wsgi import get_wsgi_application
-
-
-from whitenoise import WhiteNoise
-
-from miniblog import MyWSGIApp
-
-application = MyWSGIApp()
-application = WhiteNoise(application, root="/path/to/static/files")
-application.add_files("/path/to/more/static/files", prefix="more-files/")
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'miniblog.settings')
 
